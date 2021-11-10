fc3b1d5c1d7659cd4168f9fcaa9d23e88e0e23c4
mcaryatsalesforce <mcary@salesforce.com>
2021-11-10 15:36:11 -0600

    simplify Card with meaningful method extractions

--- src/main/java/com/jitterted/ebp/blackjack/Card.java
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -14,31 +14,56 @@ public class Card {
     }
 
     public int rankValue() {
-        if ("JQK".contains(rank)) {
+        if (isRankFaceCardNotAce()) {
             return 10;
-        } else if (rank.equals("A")) {
+        } else if (isRankAce()) {
             return 1;
         } else {
             return Integer.parseInt(rank);
         }
     }
 
-    public String display() {
-        String[] lines = new String[7];
-        lines[0] = "┌─────────┐";
-        lines[1] = String.format("│%s%s       │", rank, rank.equals("10") ? "" : " ");
-        lines[2] = "│         │";
-        lines[3] = String.format("│    %s    │", suit);
-        lines[4] = "│         │";
-        lines[5] = String.format("│       %s%s│", rank.equals("10") ? "" : " ", rank);
-        lines[6] = "└─────────┘";
+    public int rankAlternateValue() {
+        if (isRankAce()) {
+            return 11;
+        }
+        return rankValue();
+    }
 
-        Ansi.Color cardColor = "♥♦".contains(suit) ? Ansi.Color.RED : Ansi.Color.BLACK;
+    public boolean isRankAce() {
+        return rank.equals("A");
+    }
+
+    private boolean isRankFaceCardNotAce() {
+        return "JQK".contains(rank);
+    }
+
+    public String display() { // long method (something about the handling of `lines` and its formatting strikes me as cantankerous)
+        String[] lines = generateFace(this);
+
+        Ansi.Color cardColor = getCardColor();
         return ansi()
                 .fg(cardColor).toString()
                 + String.join(ansi().cursorDown(1)
                                     .cursorLeft(11)
                                     .toString(), lines);
+    }
+
+    private Ansi.Color getCardColor() {
+        return "♥♦".contains(suit) ? Ansi.Color.RED : Ansi.Color.BLACK;
+    }
+
+    private static String[] generateFace(Card card) {
+        String rankSpacing = card.rank.equals("10") ? "" : " ";
+        String[] lines = new String[7];
+        lines[0] = "┌─────────┐";
+        lines[1] = String.format("│%s%s       │", card.rank, rankSpacing);
+        lines[2] = "│         │";
+        lines[3] = String.format("│    %s    │", card.suit);
+        lines[4] = "│         │";
+        lines[5] = String.format("│       %s%s│", rankSpacing, card.rank);
+        lines[6] = "└─────────┘";
+        return lines;
     }
 
     @Override
